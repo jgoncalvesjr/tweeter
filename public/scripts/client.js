e732b23d336e3ffb04121086a7c8c5c3c64867a8
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-08-12 22:11:34 +0000

    Fixed tweet hover effect

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -27,7 +27,7 @@ const renderTweets = (tweets) => {
 const createTweetElement = (tweetObj) => {
   const date = Date(tweetObj.created_at).toString();
   let $tweet = `
-  <div class="tweet">
+  <article class="tweet">
   <header>
     <div>
       <img src="${tweetObj.user.avatars}" alt="Avatar">
@@ -44,7 +44,7 @@ const createTweetElement = (tweetObj) => {
       <fav-icon name="heart">lv</ion-icon>
     </div>
   </footer>
-</div>
+</article>
   `;
   return $tweet;
 };
@@ -95,5 +95,21 @@ $(document).ready(function() {
     });
   };
   loadTweets();
+
+  $('.tweet').on('mouseenter', function() {
+    $(this).css('box-shadow', '8px 8px #829bed');
+    $('h3.userName').css('font-weight', '400');
+    $('span.userId').css('opacity', '0.66');
+    $('fav-icon').css('font-weight', '400');
+    $('p').css('font-weight', '400');
+  });
+  
+  $('.tweet').on('mouseleave', function() {
+    $(this).css('box-shadow', 'none');
+    $('h3.userName').css('font-weight', '300');
+    $('span.userId').css('opacity', '0');
+    $('fav-icon').css('font-weight', '300');
+    $('p').css('font-weight', '300');
+  });
 
 });
